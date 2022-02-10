b877cd58f13e9f35a7f31a9a6523917c29ff4ccf
bithe <farzana.hossain5@gmail.com>
2022-02-11 00:00:05 +0600

    banking deposit and withdraw done

--- js/login.js
+++ js/login.js
@@ -1,14 +1,14 @@
-document.getElementById('login').addEventListener("click", function(){
-const email = document.getElementById('user-email');
-const userEmail = email.value;
-//password
-const password = document.getElementById('password');
-const userPassword = password.value;
-console.log(email.value);
+document.getElementById('login').addEventListener("click", function() {
+    const email = document.getElementById('user-email');
+    const userEmail = email.value;
+    //password
+    const password = document.getElementById('password');
+    const userPassword = password.value;
+    console.log(email.value);
 
-if(userEmail == 'admin@gmail.com' && userPassword == 'secret'){
-    window.location.href = 'banking.html';
-    this.getElementsByClassName
-}
+    if (userEmail == 'admin@gmail.com' && userPassword == 'secret') {
+        window.location.href = 'banking.html';
+        this.getElementsByClassName;
+    }
 
 });
