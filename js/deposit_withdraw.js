345baac7ec788c32913ec15bc2366ce6ec6557a0
bithe <farzana.hossain5@gmail.com>
2022-02-11 18:16:29 +0600

    witdr done

--- js/deposit_withdraw.js
+++ js/deposit_withdraw.js
@@ -7,9 +7,7 @@ document.getElementById('deposit-submit').addEventListener("click", function() {
     const deposit = parseFloat(depositText);
 
     console.log(deposit);
-
     //clear the deposit input field
-
     depositAmount.value = "";
     //deposit show
     const depositTotalAmount = document.getElementById('total-deposit-amount');
@@ -18,20 +16,23 @@ document.getElementById('deposit-submit').addEventListener("click", function() {
     const previousDepositTotalAmount = parseFloat(previousDepositTotalAmountText);
     const newDepositTotalAmount = previousDepositTotalAmount + deposit;
     depositTotalAmount.innerText = newDepositTotalAmount;
-    //balance
-    const balance = document.getElementById('balance');
-    balance.innerText = newDepositTotalAmount;
+
 });
 
 //Withdraw event handler
 document.getElementById('withdraw-submit').addEventListener("click", function() {
     const withdrawAmount = document.getElementById('withdraw-amount');
-    const withdraw = withdrawAmount.value;
+    const withdrawText = withdrawAmount.value;
+    const withdraw = parseFloat(withdrawText);
     console.log(withdraw);
     withdrawAmount.value = "";
     const totalWithdrawAmount = document.getElementById('total-withdraw-amount');
+    //
+    const previousTotalWithdrawText = totalWithdrawAmount.innerText;
+    const previousTotalWithdraw = parseFloat(previousTotalWithdrawText);
+    const newTotalWithdraw = previousTotalWithdraw + withdraw;
 
     // show withdraw amount in withdraw
-    totalWithdrawAmount.innerText = withdraw;
+    totalWithdrawAmount.innerText = newTotalWithdraw;
 
 });
