166f998648906221edd790b57d5eee4694c67a38
bithe <farzana.hossain5@gmail.com>
2022-02-11 10:10:32 +0600

    wuthdraw

--- js/deposit_withdraw.js
+++ js/deposit_withdraw.js
@@ -22,3 +22,16 @@ document.getElementById('deposit-submit').addEventListener("click", function() {
     const balance = document.getElementById('balance');
     balance.innerText = newDepositTotalAmount;
 });
+
+//Withdraw event handler
+document.getElementById('withdraw-submit').addEventListener("click", function() {
+    const withdrawAmount = document.getElementById('withdraw-amount');
+    const withdraw = withdrawAmount.value;
+    console.log(withdraw);
+    withdrawAmount.value = "";
+    const totalWithdrawAmount = document.getElementById('total-withdraw-amount');
+
+    // show withdraw amount in withdraw
+    totalWithdrawAmount.innerText = withdraw;
+
+});
